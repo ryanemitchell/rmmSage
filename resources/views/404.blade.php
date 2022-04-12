3bbb87f92d648a33c994ec40f57541d39c15bbcc
Ryan E. Mitchell <ryanmitchell@gmail.com>
2022-04-11 18:42:00 -0600

    404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('partials.page-header')
+    {{-- @include('partials.page-header') --}}
 
     @if (!have_posts())
         <x-alert type="warning">
             {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
         </x-alert>
 
-        @php
-            $args = [
-                'theme_location' => 'not_found_navigation',
-                'menu_id' => 'not-found-navigation',
-                'menu_class' => 'error404-nav',
-                'container_id' => 'primary_mobile_navigation',
-            ];
-            wp_nav_menu($args);
-        @endphp
+        <?php echo get_page_template(); ?>
 
-        {!! get_search_form(false) !!}
+        @if (has_nav_menu('not_found_navigation'))
+            <div id="404-menu-wrapper">
+                {!! wp_nav_menu([
+    'theme_location' => 'not_found_navigation',
+    'depth' => 1,
+    'fallback_cb' => false,
+    'menu_class' => 'grid grid-cols-1 lg:grid-cols-4 gap-4 text-center w-full',
+    'add_li_class' => 'w-full bg-blue-100 hover:bg-blue-500 cursor-pointer py-5 px-10 transition-[background] duration-500',
+]) !!}
+
+
+            </div>
+        @endif {{-- end not_found_navigation --}}
     @endif
 @endsection
