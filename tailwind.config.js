bb988c8d8f59d85a410bddbc5e08293a1ce43e37
Ryan E. Mitchell <ryanmitchell@gmail.com>
2022-04-11 18:43:57 -0600

    ⛵️ TW cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  content: ['./index.php', './app/**/*.php', './resources/**/*.{php,vue,js}'],
-  theme: {
-    extend: {
-      colors: {},
+    content: ['./index.php', './app/**/*.php', './resources/**/*.{php,vue,js}'],
+    theme: {
+      extend: {
+            colors: {},
+        },
     },
-  },
-  variants: {
-    extend: {
-      // ...
-      filter: ['hover'],
-    }
-  },
-  plugins: [
+    variants: {
+        extend: {
+          // ...
+            filter: ['hover'],
+        }
+    },
+    plugins: [
       require('tw-elements/dist/plugin')
-  ],
-  safelist: [
+    ],
+    safelist: [
     'alignFull',
-  ]
+    ]
 };
