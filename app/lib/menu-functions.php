952b979c44b2938d8f50426f6a3335a5e6a066db
Ryan E. Mitchell <ryanmitchell@gmail.com>
2022-04-11 18:47:38 -0600

    Nav function to add classes to li via menu

---
+++ app/lib/menu-functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Filename: menu-functions.php
+ * Description:
+ * Author: ryan
+ */
+
+function add_additional_class_on_li($classes, $item, $args) {
+	if(isset($args->add_li_class)) {
+		$classes[] = $args->add_li_class;
+	}
+	return $classes;
+}
+add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
