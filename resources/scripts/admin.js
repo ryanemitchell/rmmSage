d582e2941faec3c6e7aac3fda7332d0bea4e5ede
Ryan E. Mitchell <ryanmitchell@gmail.com>
2022-04-11 18:33:56 -0600

    add admin JS

---
+++ resources/scripts/admin.js
@@ -0,0 +1,27 @@
+import {domReady} from '@roots/sage/client';
+/**
+ * editor.main
+ */
+const wpwrap = (err) => {
+  if (err) {
+    // handle hmr errors
+    console.error(err);
+  }
+
+  window.Alert('admin');
+
+};
+
+/**
+ * Initialize
+ *
+ * @see https://webpack.js.org/api/hot-module-replacement
+ */
+domReady(wpwrap);
+import.meta.webpackHot?.accept(wpwrap);
+
+domReady(() => {
+
+
+
+})
