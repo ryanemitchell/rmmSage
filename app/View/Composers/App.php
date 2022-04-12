292d09adece8b8ca2e1ad4cf42a09086ac0c210f
Ryan E. Mitchell <ryanmitchell@gmail.com>
2022-04-11 18:45:52 -0600

    🧙🏻‍ add initial global blade variables

--- app/View/Composers/App.php
+++ app/View/Composers/App.php
@@ -24,6 +24,17 @@ class App extends Composer
     {
         return [
             'siteName' => $this->siteName(),
+            'businessName' => $this->businessName(),
+            'contentWidth' => "1200px",
+            'narrowWidth' => '800px',
+            'wideWidth' => '1392px',
+            'desktopBreakpoint' => "1024px",
+	        'imageAsset' => get_template_directory_uri().'/public/images/',
+            'blogTitle' => $this->blogTitle(),
+            'blogTitleElement' => $this->blogTitleElement(),
+	        'defaultFeaturedImage' => $this->defaultFeaturedImage(),
+	        'defaultFeaturedImageAlt' => $this->defaultFeaturedImage_alt(),
+            'defaultFeaturedImageTitle' => $this->defaultFeaturedImage_title(),
         ];
     }
 
@@ -36,4 +47,42 @@ class App extends Composer
     {
         return get_bloginfo('name', 'display');
     }
+
+    public function businessName()
+    {
+        return get_field('field_business_information_business_name', 'options');
+    }
+
+	public function blogTitle()
+	{
+		return get_field('field_site_settings_blog_blog_title', 'options');
+	}
+
+	public function blogTitleElement()
+	{
+		if ( get_field('field_site_settings_blog_blog_html_element', 'options') ) {
+			return get_field('field_site_settings_blog_blog_html_element', 'options');
+		} else {
+			return 'span';
+		}
+	}
+
+
+	public function defaultFeaturedImage()
+	{
+		return get_field('field_site_settings_blog_default_featuredimage', 'options');
+	}
+
+	public function defaultFeaturedImage_alt()
+	{
+		return get_field('field_site_settings_blog_default_featuredimage_alt', 'options');
+	}
+
+	public function defaultFeaturedImage_title()
+	{
+		return get_field('field_site_settings_blog_default_featuredimage_title', 'options');
+	}
+
+
+
 }
