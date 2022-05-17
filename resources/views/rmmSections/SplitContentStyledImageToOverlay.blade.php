523338162839a5cb0e5a1397d195a209db41ef82
Ryan E. Mitchell <ryanmitchell@gmail.com>
2022-05-17 17:46:48 -0600

    🖥 Desktop QA

--- resources/views/rmmSections/SplitContentStyledImageToOverlay.blade.php
+++ resources/views/rmmSections/SplitContentStyledImageToOverlay.blade.php
@@ -30,7 +30,7 @@
 		@notempty($image)
 		<div class="hidden order-1 lg:block lg:order-2 content-edge__wrapper bg-siteBlack">
 			<img src="{{$image['url']}}" alt="{{$imageALT}}" title="{{$imageTitle}}" class="image lg:clipLeftArrow"
-			     loading="lazy" height="{{$image['height']}}px" width="{{$image['width']}}px" data-aos="fade-left">
+			     loading="auto" height="{{$image['height']}}px" width="{{$image['width']}}px" data-aos="fade-left">
 		</div>
 		@endnotempty
 
