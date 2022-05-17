79807e86d0e3b6dc9a90000f3fdaa2a1270f1bfe
Ryan E. Mitchell <ryanmitchell@gmail.com>
2022-05-17 15:01:41 -0600

    📱 Mobile QA

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -7,7 +7,7 @@
         <div class="mt-4 flex flex-col items-center lg:mt-0">
             <h1 class="mb-3 text-primary">404: Page Not Found</h1>
             <p class="mb-3 text-center">Sorry, we can’t find the page you’re looking for. Questions? Give us a call!</p>
-            <div class="mb-4">@include('fieldPartials.callNow')</div>
+            <div class="mb-4 mt-4">@include('fieldPartials.callNow')</div>
 
         </div>
 
@@ -19,7 +19,7 @@
     'container' => 'nav',
     'fallback_cb' => false,
     'menu_class' => 'grid grid-cols-1 lg:grid-cols-4 gap-4 text-center w-full',
-    'add_li_class' => 'w-full cursor-pointer py-5 px-10 transition-[background] duration-500',
+    'add_li_class' => 'w-full cursor-pointer lg:py-5 px-10 transition-[background] duration-500',
 ]) !!}
             </div>
         @endif {{-- end not_found_navigation --}}
