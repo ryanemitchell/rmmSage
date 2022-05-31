5b0d1a891dadbc9ad94a45a4b2b6096e659fb37d
Ryan E. Mitchell <ryanmitchell@gmail.com>
2022-05-31 11:30:42 -0600

    🖥 📱 QA Coupon styling

--- resources/views/offers/allOffers.blade.php
+++ resources/views/offers/allOffers.blade.php
@@ -1,11 +1,14 @@
-<div class="p-4 offer p-8 bg-siteBlack shadow-common">
-	<div class="flex flex-col items-center justify-center inner__wrapper"><!-- Start of .inner__wrapper -->
+<div class="p-4 offer p-8 bg-siteBlack shadow-common lg:h-[200px]">
+	<div class="flex flex-col items-center justify-between inner__wrapper h-full"><!-- Start of .inner__wrapper -->
 		<div class="flex flex-col items-center justify-center header"><!-- Start of .header -->
-			<h2 class="text-center text-white">{{$offer->title}}</h2>
+			<h2 class="text-center text-white max-w-[70%]">{{$offer->title}}</h2>
 			<div class="subtitle text-white">{{$offer->subTitle}}</div>
 		</div><!-- End of .header -->
 		<div class="flex flex-col items-center content text-white mb-4"><!-- Start of .content -->
-			<span> @wpautokp($offer->cta)</span>
+{{--			@wpautop($offer->cta)--}}
+
+			<?php echo $offer->cta ?>
+
 		</div><!-- End of .content -->
 		<div class="flex flex-col items-center footer"><!-- Start of .footer -->
 			@notempty($offer->disclaimer)
--- resources/views/offers/featuredOffers.blade.php
+++ resources/views/offers/featuredOffers.blade.php
@@ -3,14 +3,14 @@
 	<div class="h-14 lg:h-[200px] w-full rounded-common bg-no-repeat bg-cover" style="background-image: url('{{$offer->image}}');"></div>
 {{--	<img src="{{$offer->image}}" alt="photo" class="image" loading="auto" height="400px" width="400px">--}}
 	@endnotempty
-	<div class="p-4 p-8 bg-siteBlack">
-	<div class="flex flex-col items-center justify-center inner__wrapper"><!-- Start of .inner__wrapper -->
+	<div class="p-4 p-8 bg-siteBlack lg:h-[200px]">
+	<div class="flex flex-col items-center justify-center inner__wrapper h-full justify-between"><!-- Start of .inner__wrapper -->
 		<div class="flex flex-col items-center justify-center header"><!-- Start of .header -->
-			<h2 class="text-center text-white">{{$offer->title}}</h2>
+			<h2 class="text-center text-white max-w-[70%]">{{$offer->title}}</h2>
 			<div class="subtitle text-white">{{$offer->subTitle}}</div>
 		</div><!-- End of .header -->
 		<div class="flex flex-col items-center content text-white  mb-4"><!-- Start of .content -->
-			<span> @wpautokp($offer->cta)</span>
+			<span>	<?php echo $offer->cta ?></span>
 		</div><!-- End of .content -->
 		<div class="flex flex-col items-center footer"><!-- Start of .footer -->
 			@notempty($offer->disclaimer)
