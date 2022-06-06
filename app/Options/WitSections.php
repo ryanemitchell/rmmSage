b7a879bb0895f8099fc623873a60b2cc9ac26bcd
Ryan E. Mitchell <ryanmitchell@gmail.com>
2022-06-06 15:19:59 -0600

    🖥 📱 QA Wit sections create parent in setup not ACF composer.
    https://github.com/Log1x/acf-composer/issues/93#issuecomment-954725123

--- app/Options/WitSections.php
+++ app/Options/WitSections.php
@@ -1,116 +1,116 @@
 <?php
-
-namespace App\Options;
-
-use Log1x\AcfComposer\Options as Field;
-use StoutLogic\AcfBuilder\FieldsBuilder;
-
-class WitSections extends Field
-{
-    /**
-     * The option page menu name.
-     *
-     * @var string
-     */
-    public $name = 'Wit Sections';
-
-    /**
-     * The option page menu slug.
-     *
-     * @var string
-     */
-    public $slug = 'wit-sections';
-
-    /**
-     * The option page document title.
-     *
-     * @var string
-     */
-    public $title = 'Wit Sections | Options';
-
-    /**
-     * The option page permission capability.
-     *
-     * @var string
-     */
-    public $capability = 'edit_theme_options';
-
-    /**
-     * The option page menu position.
-     *
-     * @var int
-     */
-    public $position = 2;
-
-    /**
-     * The slug of another admin page to be used as a parent.
-     *
-     * @var string
-     */
-    public $parent = null;
-
-    /**
-     * The option page menu icon.
-     *
-     * @var string
-     */
-    public $icon = null;
-
-    /**
-     * Redirect to the first child page if one exists.
-     *
-     * @var boolean
-     */
-    public $redirect = true;
-
-    /**
-     * The post ID to save and load values from.
-     *
-     * @var string|int
-     */
-    public $post = 'options';
-
-    /**
-     * The option page autoload setting.
-     *
-     * @var bool
-     */
-    public $autoload = true;
-
-    /**
-     * Localized text displayed on the submit button.
-     *
-     * @return string
-     */
-    public function updateButton()
-    {
-        return __('Update', 'acf');
-    }
-
-    /**
-     * Localized text displayed after form submission.
-     *
-     * @return string
-     */
-    public function updatedMessage()
-    {
-        return __('Wit Sections Updated', 'acf');
-    }
-
-    /**
-     * The option page field group.
-     *
-     * @return array
-     */
-    public function fields()
-    {
-        $witSections = new FieldsBuilder('wit_sections');
-
-        $witSections
-            ->addRepeater('items')
-                ->addText('item')
-            ->endRepeater();
-
-        return $witSections->build();
-    }
-}
+//
+//namespace App\Options;
+//
+//use Log1x\AcfComposer\Options as Field;
+//use StoutLogic\AcfBuilder\FieldsBuilder;
+//
+//class WitSections extends Field
+//{
+//    /**
+//     * The option page menu name.
+//     *
+//     * @var string
+//     */
+//    public $name = 'Wit Sections';
+//
+//    /**
+//     * The option page menu slug.
+//     *
+//     * @var string
+//     */
+//    public $slug = 'wit-sections';
+//
+//    /**
+//     * The option page document title.
+//     *
+//     * @var string
+//     */
+//    public $title = 'Wit Sections | Options';
+//
+//    /**
+//     * The option page permission capability.
+//     *
+//     * @var string
+//     */
+//    public $capability = 'edit_theme_options';
+//
+//    /**
+//     * The option page menu position.
+//     *
+//     * @var int
+//     */
+//    public $position = 2;
+//
+//    /**
+//     * The slug of another admin page to be used as a parent.
+//     *
+//     * @var string
+//     */
+//    public $parent = null;
+//
+//    /**
+//     * The option page menu icon.
+//     *
+//     * @var string
+//     */
+//    public $icon = null;
+//
+//    /**
+//     * Redirect to the first child page if one exists.
+//     *
+//     * @var boolean
+//     */
+//    public $redirect = true;
+//
+//    /**
+//     * The post ID to save and load values from.
+//     *
+//     * @var string|int
+//     */
+//    public $post = 'options';
+//
+//    /**
+//     * The option page autoload setting.
+//     *
+//     * @var bool
+//     */
+//    public $autoload = true;
+//
+//    /**
+//     * Localized text displayed on the submit button.
+//     *
+//     * @return string
+//     */
+//    public function updateButton()
+//    {
+//        return __('Update', 'acf');
+//    }
+//
+//    /**
+//     * Localized text displayed after form submission.
+//     *
+//     * @return string
+//     */
+//    public function updatedMessage()
+//    {
+//        return __('Wit Sections Updated', 'acf');
+//    }
+//
+//    /**
+//     * The option page field group.
+//     *
+//     * @return array
+//     */
+//    public function fields()
+//    {
+//        $witSections = new FieldsBuilder('wit_sections');
+//
+//        $witSections
+//            ->addRepeater('items')
+//                ->addText('item')
+//            ->endRepeater();
+//
+//        return $witSections->build();
+//    }
+//}
